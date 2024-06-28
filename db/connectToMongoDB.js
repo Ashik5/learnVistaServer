45d982153de45290c706dbdbf00f4d295d789867
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-29 01:56:13 +0600

    cors

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
+        await mongoose.connect(process.env.MONGO_DB_URI,{
+            dbName: "learnVista",
+        })
         console.log("MongoDB is Connected")
     } catch (error) {
         console.log("Error Connecting to DB", error.message)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import Book from './models/book.model.js';
+import cors from "cors";
 
 
 dotenv.config();
@@ -10,9 +11,20 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 
-app.listen(() => {
+app.listen(PORT, () => {
     connectToMongoDB();
     app.get("/", (req, res) => {
         res.send("Hello World");
     });
+})
+app.use(cors(
+    {
+        origin: ["deployed-vercel-frontend-app","localhost:3000"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+
+app.get("/", (req, res) => {
+  res.json("Hello");
 })
