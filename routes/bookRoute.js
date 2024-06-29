978081359ae506515c293aeb7cc1065f2d3df6ee
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-29 23:51:24 +0600

    vercel config

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 import Book from '../models/book.model.js';
 
 router.get("/", (req, res) => {
-   res.send("working backend");
+    Book.find(
+        {},
+    ).then((books) => { res.json(books) }).catch((err) => { res.status(400).json("Error: " + err) });
 });
 
 export default router;
