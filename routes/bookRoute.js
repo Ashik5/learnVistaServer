4a179ddf7b62140f92d9990c47ba4557e15a7b9e
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-29 23:46:57 +0600

    vercel config

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,18 +3,7 @@ const router = express.Router();
 import Book from '../models/book.model.js';
 
 router.get("/", (req, res) => {
-    Book.find(
-        {},
-    ).then((books) => { res.json(books) }).catch((err) => { res.status(400).json("Error: " + err) });
-});
-router.post("/", async(req, res) => {
-    const book = new Book(req.body);
-    try {
-        await book.save();
-        res.json("Book added successfully");
-    } catch (err) {
-        res.status(400).json("Error: " + err);
-    }
+   res.send("working backend");
 });
 
 export default router;
