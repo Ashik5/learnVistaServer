99a9d298d57a3e46c69b4fd175e1e0816e5b4d8a
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-29 16:35:45 +0600

    book model setup

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 import Book from '../models/book.model.js';
 
-router.get("/", async(req, res) => {
-    await Book.find(
+router.get("/", (req, res) => {
+    Book.find(
         {},
     ).then((books) => { res.json(books) }).catch((err) => { res.status(400).json("Error: " + err) });
 });
