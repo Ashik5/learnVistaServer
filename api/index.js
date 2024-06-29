45b8de996ddeafc304a18dff38f9e4275ba8b6a3
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-29 23:37:14 +0600

    vercel config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,40 @@
-import app from "./api/index.js";
+import express from "express"
+import dotenv from "dotenv"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cors from "cors";
+
+import bookRoute from "./routes/bookRoute.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    app.get("/", (req, res) => {
+        res.send("Hello World");
+    });
+})
+app.use(cors(
+    {
+        origin: ["deployed-vercel-frontend-app","localhost:3000"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.get("/", (req, res) => {    
+    try{
+        Book.find(
+            {},
+        ).then((books) => { res.json(books) });
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+}
+);
+app.use("/books", bookRoute);
 
 export default app;
