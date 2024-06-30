624cf888b0177a843b14ec29bec698e3efb885f5
Ashik Mahmud <ashikmamud123@gmail.com>
2024-06-30 11:56:36 +0600

    backend cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cors from "cors";
 
 import bookRoute from "./routes/bookRoute.js";
-
+app.use(express.json());
 app.use("",cors(
     {
         origin: "*",
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.json());
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
