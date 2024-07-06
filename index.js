d0d795252f5389ab9f54e1b15aeab02d341f9110
Ashik Mahmud <ashikmamud123@gmail.com>
2024-07-07 03:11:23 +0600

    bycript problem

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
 import authRouter from "./routes/auth.js";
-import log from "./middlewares/logger.js";
 import cookieParser from 'cookie-parser';
 
 import bookRoute from "./routes/bookRoute.js";
@@ -22,7 +21,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(log);
 
 app.listen(PORT, () => {
     connectToMongoDB();
