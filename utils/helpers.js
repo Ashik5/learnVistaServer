5dc31b06396a1c9ba758703a774a9221729e5a59
Ashik Mahmud <ashikmamud123@gmail.com>
2024-07-07 03:21:55 +0600

    bycript problem

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,14 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 
 const saltRounds = 10;
 
 export const hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hash = await bcrypt.hash(password, salt);
+  const salt = await bcryptjs.genSalt(saltRounds);
+  const hash = await bcryptjs.hash(password, salt);
   return hash;
 };
 
 export const comparePassword = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword);
+  return await bcryptjs.compare(password, hashedPassword);
 };
 
