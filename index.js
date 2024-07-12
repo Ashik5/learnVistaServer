693f56d25f06614440fde95ea5f3d8a6c8b6f02f
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2024-07-13 02:43:29 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import bookRoute from "./routes/bookRoute.js";
 const app = express();
 app.use("",cors(
     {
-        origin: "http://localhost:3000",
+        origin: "https://learnvista.vercel.app",
         credentials: true,
     }
 ));
