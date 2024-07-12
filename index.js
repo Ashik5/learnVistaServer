8f434267842648a8d24fb1788839aba215900e8a
Ashik5 <ashikmamud123@gmail.com>
2024-07-13 02:04:16 +0600

    image upload

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import cors from "cors";
 import userRoutes from "./routes/users.js";
 import authRouter from "./routes/auth.js";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import bookRoute from "./routes/bookRoute.js";
 const app = express();
 app.use("",cors(
     {
-        origin: "https://learnvista.vercel.app",
+        origin: "https://learnvista.vercel.app/",
         credentials: true,
     }
 ));
@@ -21,16 +22,17 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
 
 app.listen(PORT, () => {
     connectToMongoDB();
     app.get("/", (req, res) => {
-        res.send("Hello World");
+        res.send(process.env.CLIENT_URL);
     });
 })
 
 app.get("/", (req, res) => {    
-    res.send("Hello World");
+    res.send(process.env.CLIENT_URL);
 }
 );
 app.use("/users", userRoutes);
