ac600b08d791a8ecc0a7e8ce72cf3e2f016d65f2
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2024-09-22 12:08:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://learnvista.vercel.app",
     credentials: true,
 }));
 const PORT = process.env.PORT || 5000;
